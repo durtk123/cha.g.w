f3eff38a5b115e8c19a10d12b1e1371a3d0cd2c1
durtk123 <149846632+durtk123@users.noreply.github.com>
2023-11-11 09:51:04 +0900

    Update gwfg.py

--- gwfg.py
+++ gwfg.py
@@ -1,10 +1,4 @@
-import streamlit as st
 import pandas as pd
-st.metric(label="온도", value="10℃", delta="1.2℃")
-st.metric(label="삼성전자", value="61,000원", delta="-1,200 원")
-
-col1, col2, col3 = st.columns(3)
-col1.metric(label="달러USD", value="1,228원", delta="-12.00원")
 
 import streamlit as st
 
